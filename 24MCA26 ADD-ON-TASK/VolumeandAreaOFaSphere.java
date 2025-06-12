a1fff8c11e5df4041e679f8a90a044b2e94a8941
Gokul K <24mca26@kgisliim.ac.in>
2025-06-12 07:10:04 +0530

    Update VolumeandAreaOFaSphere.java

--- 24MCA26 ADD-ON-TASK/VolumeandAreaOFaSphere.java
+++ 24MCA26 ADD-ON-TASK/VolumeandAreaOFaSphere.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AreaVolumeSphere {
+public class VolumeandAreaOFaSphere {
 	public static void main(String[] args) {
 		 Scanner scanner = new Scanner(System.in);
 	        
