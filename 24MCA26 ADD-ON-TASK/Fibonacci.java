8efefd32d811ca07dcb0fb3754838ce1fc1bd5ae
Gokul K <24mca26@kgisliim.ac.in>
2025-06-12 07:15:15 +0530

    Update and rename Fibonacci .java to Fibonacci.java

--- 24MCA26 ADD-ON-TASK/Fibonacci.java
+++ 24MCA26 ADD-ON-TASK/Fibonacci.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FibonacciSeries {
+public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("How many Fibonacci terms? ");
