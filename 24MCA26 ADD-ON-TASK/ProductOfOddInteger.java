3175438504765dff30c9f50bb52461a110678ebe
Gokul K <24mca26@kgisliim.ac.in>
2025-06-12 08:57:41 +0530

    Update ProductOfOddInteger.java

--- 24MCA26 ADD-ON-TASK/ProductOfOddInteger.java
+++ 24MCA26 ADD-ON-TASK/ProductOfOddInteger.java
@@ -1,6 +1,18 @@
 
 public class ProductOfOddInteger {
 	public static void main(String[] args) {
-		
-	}
+        long product = 1;
+        String series = "";
+
+        for (int i = 1; i <= 15; i += 2) {
+            product *= i;
+            series += i;
+            if (i < 15) {
+                series += " × ";
+            }
+        }
+
+        System.out.println("Product of odd numbers from 1 to 15 = " + product);
+        System.out.println("(Odd numbers: " + series + " = " + product + ")");
+    }
 }
